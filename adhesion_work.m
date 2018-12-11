ee1f0b87322ef1b0fbd6fb3a5f524f46a9b8894f
Babak <25906261+babaknasr@users.noreply.github.com>
2018-12-11 13:24:12 -0500

    Update adhesion_work.m

--- adhesion_work.m
+++ adhesion_work.m
@@ -1,11 +1,10 @@
 % Work of adhesion from Hamaker Constant
 function Wa = adhesion_work(varargin)
-
-A = varargin{1};
+A = varargin{1}; % Hamake
 if (length(varargin) == 2)
     z0 = varargin{2};
 else
-    z0 = 4e-10;
+    z0 = 4e-10; % Minimum separation distance (Air)
 end %if
 
 Wa = A/(12*pi*z0^2);
